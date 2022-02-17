4320381af4f5e0bc17d4b4d0345a44e2fd2c36a3
s00188563 <fernandezluigi10@yahoo.ie>
2022-02-17 21:46:44 +0000

    can see all books

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FirebaseApiService } from './services/firebase-api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'firebase-sf-app';
+  MyBooks: any = [];
+  titleValue = '';
+  authorValue = '';
+
+  constructor(public firebaseApiService: FirebaseApiService) {}
+
+  ngOnInit(): void {
+    this.loadBooks();
+  }
+
+  loadBooks() {
+    return this.firebaseApiService.getBooks().subscribe((data: {}) => {
+      this.MyBooks = data;
+    });
+  }
 }
---
+++ src/app/services/firebase-api.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Book } from '../interfaces/book';
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FirebaseApiService {
+  apiURL = 'https://us-central1-bookshelf-lf.cloudfunctions.net';
+
+  constructor(private http: HttpClient) {}
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    }),
+  };
+
+  getBooks(): Observable<Book> {
+    return this.http
+      .get<Book>(this.apiURL + '/getBooks')
+      .pipe(retry(1), catchError(this.handleError));
+  }
+
+  handleError(error) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = error.error.message;
+    } else {
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    window.alert(errorMessage);
+    return throwError(errorMessage);
+  }
+}
